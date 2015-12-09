2a47c840af35bc1c7642ff509934917bd4f1f57d
Inma <Inma@Inma-PC>
2015-12-09 10:16:00 +0100

    Ejercicio8

---
+++ baseDatosEj8Pag85/src/baseDatosEj83/BdEje8Derby.java
@@ -0,0 +1,53 @@
+package baseDatosEj83;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BdEje8Derby {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("org.apache.derby.jdbc.EmbeddedDriver"); // cargar el
+																	// driver
+			// Establecemos la conexion con la BD
+			Connection conexion = DriverManager.getConnection("jdbc:derby://localhost:1527/dracof", "dracof", "dracof");
+
+			DatabaseMetaData dbmd = conexion.getMetaData(); // Creamos objeto
+			ResultSet resul = null;
+
+			String nombre = dbmd.getDatabaseProductName();
+			String driver = dbmd.getDriverName();
+			String url = dbmd.getURL();
+			String usuario = dbmd.getUserName();
+
+			System.out.println("INFROMACIÓN SOBRE LA BASE DE DATOS:");
+			System.out.println("===================================");
+			System.out.println("Nombre 	: " + nombre);
+			System.out.println("Driver 	: " + driver);
+			System.out.println("URL 	: " + url);
+			System.out.println("Usuario : " + usuario);
+
+			// Obtener información de las tablas y vistas que hay
+			resul = dbmd.getTables(null, null, null, new String[]{"TABLE"});
+
+			while (resul.next()) {
+				String catalogo = resul.getString(1); // columna 1 que devuelve
+														// ResulSet
+				String esquema = resul.getString(2); // columna 2
+				String tabla = resul.getString(3); // columna 3
+				String tipo = resul.getString(4); // columna 4
+				System.out.println(tipo + " - Catalogo: " + catalogo + ", Esquema : " + esquema + ", Nombre: " + tabla);
+			}
+
+			conexion.close(); // cerrar conexion
+		} catch (ClassNotFoundException cn) {
+			cn.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
